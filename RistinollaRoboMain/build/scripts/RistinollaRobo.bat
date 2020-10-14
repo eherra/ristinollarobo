02f9b6c7a34bf5293752d4ee9bcbecb40d996911
Elias Herranen <elias.herranen@gmail.com>
2020-10-14 20:13:44 +0300

    testi

--- RistinollaRoboMain/build/scripts/RistinollaRobo.bat
+++ RistinollaRoboMain/build/scripts/RistinollaRobo.bat
@@ -1,3 +1,19 @@
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -66,7 +82,7 @@ set CMD_LINE_ARGS=%*
 set CLASSPATH=%APP_HOME%\lib\RistinollaRobo.jar
 
 @rem Execute RistinollaRobo
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %RISTINOLLA_ROBO_OPTS%  -classpath "%CLASSPATH%" RistinollaRobo.Main %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %RISTINOLLA_ROBO_OPTS%  -classpath "%CLASSPATH%" RistinollaRobo.ui.Main %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
